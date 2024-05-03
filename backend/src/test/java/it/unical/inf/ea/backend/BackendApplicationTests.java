91d8bc992a0299b72c21a195402230a6c79002e9
gaetano <gaetanosj9@gmail.com>
2024-05-03 15:23:21 +0200

    aggiunta backend

---
+++ backend/src/test/java/it/unical/inf/ea/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.inf.ea.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
