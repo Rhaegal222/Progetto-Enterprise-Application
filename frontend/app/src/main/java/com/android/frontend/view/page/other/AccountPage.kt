00e011ce13b698466f30f89a44ad3900265645e3
Rhaegal222 <rhaegal222@gmail.com>
2024-06-28 20:05:47 +0200

    Refactor AccountPage to improve code readability and maintainability

--- frontend/app/src/main/java/com/android/frontend/view/page/other/AccountPage.kt
+++ frontend/app/src/main/java/com/android/frontend/view/page/other/AccountPage.kt
@@ -24,9 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.android.frontend.view_models.ProfileViewModel
