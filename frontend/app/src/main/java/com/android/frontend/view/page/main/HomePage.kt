e089bd818aa4774eb6b351baa95b5c166a306e8e
Rhaegal222 <rhaegal222@gmail.com>
2024-06-30 07:13:43 +0200

    chore: Implement lazy loading for homepage image previews

--- frontend/app/src/main/java/com/android/frontend/view/page/main/HomePage.kt
+++ frontend/app/src/main/java/com/android/frontend/view/page/main/HomePage.kt
@@ -1,10 +1,9 @@
 package com.android.frontend.view.page.main
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -21,17 +20,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.android.frontend.R
 import com.android.frontend.view.component.Suggestion
 import com.android.frontend.view_models.HomeViewModel
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.delay
 
+@OptIn(ExperimentalPagerApi::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomePage(navController: NavHostController, homeViewModel: HomeViewModel = viewModel()) {
-
-    val viewModel: HomeViewModel = viewModel()
 
     var searchQuery by remember { mutableStateOf("") }
     var focusOnTextField by remember { mutableStateOf(false) }
@@ -122,15 +125,51 @@ fun HomePage(navController: NavHostController, homeViewModel: HomeViewModel = vi
             if (focusOnTextField) {
                 Suggestion()
             } else {
-                HomePagePreview()
+                HomePagePreview(rememberPagerState(initialPage = 0))
             }
         }
     }
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun HomePagePreview() {
-    HorizontalPager(state = rememberPagerState(pageCount = { 2 })) {
+fun HomePagePreview(pagerState: com.google.accompanist.pager.PagerState) {
 
+    var currentPage by remember { mutableIntStateOf(0) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(10000)
+            currentPage = (currentPage + 1) % 3
+            tween<Float>(2000)
+            pagerState.animateScrollToPage(page = currentPage)
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
+
+        Text(text = "Offerte del giorno")
+
+        HorizontalPager(
+            state = pagerState,
+            count = 3,
+            modifier = Modifier.fillMaxWidth()
+        ) { page ->
+            when (page) {
+                0 -> {
+                    Text(text = "Pagina 1")
+                }
+                1 -> {
+                    Text(text = "Pagina 2")
+                }
+                2 -> {
+                    Text(text = "Pagina 3")
+                }
+            }
+        }
     }
 }
