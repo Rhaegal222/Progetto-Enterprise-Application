f1b9f3f424dbb4d23fb7970863e2e819137ea6aa
PeppeL <giuseppe.lentini1021@gmail.com>
2024-07-04 23:07:27 +0200

    edit Order classes in the frontend

--- frontend/app/src/main/java/com/android/frontend/dto/OrderDTO.kt
+++ frontend/app/src/main/java/com/android/frontend/dto/OrderDTO.kt
@@ -1,17 +1,19 @@
 package com.android.frontend.dto
 
+import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.util.UUID
 
 data class OrderDTO(
     val id: UUID,
-    val userId: Long,
-    val addressId: Long,
-    val paymentMethodId: Long,
-    val items: List<CartItemDTO>,
-    val status: OrderStatus,
+    val items: List<OrderItemDTO>,
+    val totalCost: BigDecimal,
+    val status: String,
     val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime
+    val updatedAt: LocalDateTime,
+    val user: UserDTO,
+    val address: AddressDTO,
+    val paymentMethod: PaymentMethodDTO
 ){
     enum class OrderStatus {
         CREATED,
--- frontend/app/src/main/java/com/android/frontend/dto/creation/OrderCreateDTO.kt
+++ frontend/app/src/main/java/com/android/frontend/dto/creation/OrderCreateDTO.kt
@@ -1,14 +1,8 @@
 package com.android.frontend.dto.creation
 
-import com.android.frontend.dto.AddressDTO
-import com.android.frontend.dto.CartDTO
-import com.android.frontend.dto.CartItemDTO
-import com.android.frontend.dto.PaymentMethodDTO
 import java.util.UUID
 
 data class OrderCreateDTO(
-    val items : List<CartItemDTO>,
-    val addressId : UUID,
-    val paymentMethod : UUID,
-    val userId : UUID
+    val addressId: UUID,
+    val paymentMethodId: UUID
 )
