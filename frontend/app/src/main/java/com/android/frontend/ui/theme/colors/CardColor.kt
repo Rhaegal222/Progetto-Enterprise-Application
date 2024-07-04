56f93d40248b538810bb126d8776429d297195e0
Rhaegal222 <rhaegal222@gmail.com>
2024-07-04 22:48:06 +0200

    chore: Add default address string to strings.xml

--- frontend/app/src/main/java/com/android/frontend/ui/theme/colors/CardColor.kt
+++ frontend/app/src/main/java/com/android/frontend/ui/theme/colors/CardColor.kt
@@ -1,2 +1,30 @@
 package com.android.frontend.ui.theme.colors
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+object CardColorScheme {
+
+    val containerColorLight = Color(0xFFC0C0C0) // Grigio chiaro per il contenitore
+    val contentColorLight = Color(0xFF000000)   // Nero per il contenuto
+    val disabledContainerColorLight = Color(0xFFB0B0B0) // Grigio medio per il contenitore disabilitato
+    val disabledContentColorLight = Color(0xFF808080)   // Grigio scuro per il contenuto disabilitato
+    val containerColorDark = Color(0xFF000000)
+    val contentColorDark = Color(0xFFFFFFFF)
+    val disabledContainerColorDark = Color(0xFF424242)
+    val disabledContentColorDark = Color(0xFF757575)
+    @Composable
+    fun cardColors(
+        containerColor: Color = if (isSystemInDarkTheme()) containerColorDark else containerColorLight,
+        contentColor: Color = if (isSystemInDarkTheme()) contentColorDark else contentColorLight,
+        disabledContainerColor: Color = if (isSystemInDarkTheme()) disabledContainerColorDark else disabledContainerColorLight,
+        disabledContentColor: Color = if (isSystemInDarkTheme()) disabledContentColorDark else disabledContentColorLight
+    ) = CardDefaults.cardColors(
+        containerColor = containerColor,
+        contentColor = contentColor,
+        disabledContainerColor = disabledContainerColor,
+        disabledContentColor = disabledContentColor
+    )
+}
